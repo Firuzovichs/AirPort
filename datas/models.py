6cc92c5d95e713110600696f99a23bd2ded1fedb
Firuzovichs <firuzovichs@gmail.com>
2025-02-17 14:48:17 +0500

    xatolar

--- datas/models.py
+++ datas/models.py
@@ -6,8 +6,8 @@ class Dispatch(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.ForeignKey(TypeModel, on_delete=models.CASCADE, null=True,blank=True)
     tranzit = models.ForeignKey(TransitModel, on_delete=models.CASCADE,null=True,blank=True)
-    sex_1 = models.ForeignKey(SexModel, on_delete=models.CASCADE, related_name='otkuda',default=None)
-    sex_2 = models.ForeignKey(SexModel, on_delete=models.CASCADE, related_name='kuda', default=None)
+    sex_1 = models.ForeignKey(SexModel, null=True, blank=True, on_delete=models.CASCADE, related_name='otkuda',default=None)
+    sex_2 = models.ForeignKey(SexModel, null=True, blank=True, on_delete=models.CASCADE, related_name='kuda', default=None)
     data_reception = models.DateTimeField(null=True,blank=True)
     data_dispatch = models.DateTimeField(null=True,blank=True)
     dispatches = models.CharField(max_length=255,null=True,blank=True)
@@ -15,12 +15,14 @@ class Dispatch(models.Model):
     to_capital = models.ForeignKey(Capital, on_delete=models.CASCADE,null=True,blank=True, related_name="otkuda_capital")
     from_country = models.ForeignKey(Country, on_delete=models.CASCADE,null=True,blank=True, related_name="kuda_country")
     from_capital = models.ForeignKey(Capital, on_delete=models.CASCADE,null=True,blank=True, related_name="kuda_capital")
-    status = models.ForeignKey(StatusModel, on_delete=models.CASCADE, null=True,blank=True)
+    status = models.ForeignKey(StatusModel, null=True, blank=True, on_delete=models.SET_NULL)
     category_status = models.ForeignKey(CategoryMailModel, on_delete=models.CASCADE, default=None,null=True,blank=True)
     quantity = models.IntegerField(null=True,blank=True)
     weight = models.FloatField(null=True,blank=True)
-    note = models.TextField(default=None)
+    note = models.TextField(null=True, blank=True)
     flightNumber = models.CharField(max_length=255, null=True)
 
     def __str__(self):
-        return f" {self.status.id} ({self.quantity} items, {self.weight} kg)"
+        if self.status is not None:
+            return f" {self.status.id} ({self.quantity} ta, {self.weight} kg)"
+        return f"Status belgilanmagan ({self.quantity} ta, {self.weight} kg)"
--- datas/serializers.py
+++ datas/serializers.py
@@ -1,39 +1,46 @@
 from rest_framework import serializers
 from .models import Dispatch
 
+# class DispatchSerializer(serializers.ModelSerializer):
+#     tranzit = serializers.SerializerMethodField()
+#     sex_1 = serializers.SerializerMethodField()
+#     sex_2 = serializers.SerializerMethodField()
+#     to_country = serializers.SerializerMethodField()
+#     to_capital = serializers.SerializerMethodField()
+#     from_country = serializers.SerializerMethodField()
+#     from_capital = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = Dispatch
+#         fields = [
+#             "tranzit", "sex_1", "sex_2", "data_reception", "data_dispatch",
+#             "to_country", "to_capital", "from_country", "from_capital"
+#         ]
+
+#     def get_tranzit(self, obj):
+#         return obj.tranzit.name if obj.tranzit else None
+
+#     def get_sex_1(self, obj):
+#         return obj.sex_1.name if obj.sex_1 else None
+
+#     def get_sex_2(self, obj):
+#         return obj.sex_2.name if obj.sex_2 else None
+
+#     def get_to_country(self, obj):
+#         return obj.to_country.name if obj.to_country else None
+
+#     def get_to_capital(self, obj):
+#         return obj.to_capital.name if obj.to_capital else None
+
+#     def get_from_country(self, obj):
+#         return obj.from_country.name if obj.from_country else None
+
+#     def get_from_capital(self, obj):
+#         return obj.from_capital.name if obj.from_capital else None
 class DispatchSerializer(serializers.ModelSerializer):
-    tranzit = serializers.SerializerMethodField()
-    sex_1 = serializers.SerializerMethodField()
-    sex_2 = serializers.SerializerMethodField()
-    to_country = serializers.SerializerMethodField()
-    to_capital = serializers.SerializerMethodField()
-    from_country = serializers.SerializerMethodField()
-    from_capital = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Dispatch
-        fields = [
-            "tranzit", "sex_1", "sex_2", "data_reception", "data_dispatch",
-            "to_country", "to_capital", "from_country", "from_capital"
-        ]
-
-    def get_tranzit(self, obj):
-        return obj.tranzit.name if obj.tranzit else None
-
-    def get_sex_1(self, obj):
-        return obj.sex_1.name if obj.sex_1 else None
-
-    def get_sex_2(self, obj):
-        return obj.sex_2.name if obj.sex_2 else None
-
-    def get_to_country(self, obj):
-        return obj.to_country.name if obj.to_country else None
-
-    def get_to_capital(self, obj):
-        return obj.to_capital.name if obj.to_capital else None
-
-    def get_from_country(self, obj):
-        return obj.from_country.name if obj.from_country else None
-
-    def get_from_capital(self, obj):
-        return obj.from_capital.name if obj.from_capital else None
+        fields = "__all__"
+        
